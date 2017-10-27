7c7177e61bed7b03c8bf2e65b7ca845fef0a8a0f
euwbah <euwbah@gmail.com>
2017-10-27 19:55:39 +0800

    Finish translating auto-cookies

--- app/src/main/java/com/spcore/LoginActivity.java
+++ app/src/main/java/com/spcore/LoginActivity.java
@@ -3,6 +3,7 @@ package com.spcore;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
--- spmobileapi/src/main/kotlin/com/spcore/spmobileapi/helpers/AutoCookies.kt
+++ spmobileapi/src/main/kotlin/com/spcore/spmobileapi/helpers/AutoCookies.kt
@@ -1,5 +1,10 @@
 package com.spcore.spmobileapi.helpers
 
+import android.content.SharedPreferences
+import android.net.Uri
+import okhttp3.Interceptor
+import okhttp3.Response
+
 /*
     Helpers in this file allows the Retrofit/HttpOk3 to automatically handle cookies
     by saving cookies (in KVP map form) taken from Set-Cookie response headers and
@@ -18,5 +23,59 @@ package com.spcore.spmobileapi.helpers
     solution, it is CRUCIAL to provide an option in the Settings Activity
     to clear the App Cache, in the event that some cookies are corrupted / something
     wrong with this whole system.
+
+    Kudos to @twiceyuan for the original Java version of this
+    https://gist.github.com/twiceyuan/909331b966a63cf9538985e697e2c148
  */
 
+class CookiesAddInterceptor(private val cookieStore: CookieStore) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val url = chain.request().url().toString()
+        val host = Uri.parse(url).host
+        val builder = chain.request().newBuilder()
+        val preferences = cookieStore.getCookies(host)
+
+        preferences?.let {
+            for (cookie in it)
+                builder.addHeader("Cookie", cookie)
+        }
+
+        return chain.proceed(builder.build())
+    }
+}
+
+class CookiesRecInterceptor(private val cookieStore: CookieStore) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val url = chain.request().url().toString()
+        val host = Uri.parse(url).host
+        val originalResponse = chain.proceed(chain.request())
+
+        originalResponse.headers("Set-Cookie").let {
+            if (it.isNotEmpty())
+                cookieStore.setCookies(host, HashSet(it))
+        }
+
+        return originalResponse
+    }
+}
+
+class CookieStore(private val cookieSP: SharedPreferences) {
+    fun setCookies(host: String, cookies: Set<String>) {
+        synchronized(this) {
+            cookieSP.edit().putStringSet(host, cookies).apply()
+        }
+    }
+
+    fun getCookiesString(host: String): String {
+        synchronized(this) {
+            return cookieSP.getStringSet(host, null)
+                    ?.reduce { acc, x -> "$acc; $x"} ?: ""
+        }
+    }
+
+    fun getCookies(host: String): Set<String>? {
+        synchronized(this) {
+            return cookieSP.getStringSet(host, null)
+        }
+    }
+}
