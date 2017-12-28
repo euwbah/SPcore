fb0a20967124de16f6e06c897c74ae33274281f7
euwbah <euwbah@gmail.com>
2017-12-29 00:55:46 +0800

    Start work on event details page

---
+++ app/src/main/kotlin/com/spcore/activities/EventDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.spcore.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class EventDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_details)
+    }
+}
