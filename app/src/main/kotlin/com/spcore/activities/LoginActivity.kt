dd77f04cb157b7cc4ab167a3773de03a1e2b9a7b
euwbah <euwbah@gmail.com>
2018-01-01 20:01:59 +0800

    Fixed issue of InitialLogin Activity's Constraint Layout going berserk
    The issue only came up when the soft input keyboard was open whilst going from the LoginActivity to the InitialLoginActivity

--- app/src/main/kotlin/com/spcore/activities/LoginActivity.kt
+++ app/src/main/kotlin/com/spcore/activities/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.spcore.activities
 import android.annotation.TargetApi
 import android.content.Context
 import android.content.Intent
+import android.inputmethodservice.InputMethodService
 
 import android.os.AsyncTask
 
@@ -12,6 +13,7 @@ import android.text.TextUtils
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
 import android.widget.Toast
 
@@ -23,6 +25,7 @@ import com.spcore.apis.LoginResponse
 import com.spcore.helpers.onAnimationEnd
 import com.spcore.spmobileapi.SPMobileAPI
 import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.contentView
 
 class LoginActivity : AppStateTrackerActivity("LoginActivity") {
     /**
@@ -102,6 +105,15 @@ class LoginActivity : AppStateTrackerActivity("LoginActivity") {
             // perform the user login attempt.
             showProgress(true)
             val adminNoStrWithoutP = adminNoStr.replace("p", "", true)
+
+            // Make sure to clear focus and hide keyboard before attempting to log in
+            // This fixes a bug in which the constraint layout will screw up
+            // if the soft input is open whilst moving between activities.
+
+            focusView?.clearFocus()
+            (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+                    .hideSoftInputFromWindow(contentView?.windowToken, 0)
+
             mAuthTask = UserLoginTask(adminNoStrWithoutP, passwordStr)
             mAuthTask?.execute()
         }
