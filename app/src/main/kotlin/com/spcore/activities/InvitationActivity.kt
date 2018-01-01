5496060cb289e2baa1ddb7969f0b950ad2c95769
budisyahiddin <budisyahiddin@protonmail.ch>
2018-01-02 01:05:27 +0800

    😄 😆 😊 😃 ☺️ 😏 😍 😘 😚 😳 😌 😆 😁 😉 😜 😝 😀 😗 😙 😛 😴 😟 😦 😧 😮 😬 😕 😯 😑😄 😆 😊 😃 ☺️ 😏 😍 😘 😚 😳 😌 😆 😁 😉 😜 😝 😀 😗 😙 😛 😴 😟 😦 😧 😮 😬 😕 😯 😑😄 😆 😊 😃 ☺️ 😏 😍 😘 😚 😳 😌 😆 😁 😉 😜 😝 😀 😗 😙 😛 😴 😟 😦 😧 😮 😬 😕 😯 😑😄 😆 😊 😃 ☺️ 😏 😍 😘 😚 😳 😌 😆 😁 😉 😜 😝 😀 😗 😙 😛 😴 😟 😦 😧 😮 😬 😕 😯 😑😄 😆 😊 😃 ☺️ 😏 😍 😘 😚 😳 😌 😆 😁 😉 😜 😝 😀 😗 😙 😛 😴 😟 😦 😧 😮 😬 😕 😯 😑😄 😆 😊 😃 ☺️ 😏 😍 😘 😚 😳 😌 😆 😁 😉 😜 😝 😀 😗 😙 😛 😴 😟 😦 😧 😮 😬 😕 😯 😑😄 😆 😊 😃 ☺️ 😏 😍 😘 😚 😳 😌 😆 😁 😉 😜 😝 😀 😗 😙 😛 😴 😟 😦 😧 😮 😬 😕 😯 😑😄 😆 😊 😃 ☺️ 😏 😍 😘 😚 😳 😌 😆 😁 😉 😜 😝 😀 😗 😙 😛 😴 😟 😦 😧 😮 😬 😕 😯 😑😄 😆 😊 😃 ☺️ 😏 😍 😘 😚 😳 😌 😆 😁 😉 😜 😝 😀 😗 😙 😛 😴 😟 😦 😧 😮 😬 😕 😯 😑😄 😆 😊 😃 ☺️ 😏 😍 😘 😚 😳 😌 😆 😁 😉 😜 😝 😀 😗 😙 😛 😴 😟 😦 😧 😮 😬 😕 😯 😑😄 😆 😊 😃 ☺️ 😏 😍 😘 😚 😳 😌 😆 😁 😉 😜 😝 😀 😗 😙 😛 😴 😟 😦 😧 😮 😬 😕 😯 😑

--- app/src/main/kotlin/com/spcore/activities/InvitationActivity.kt
+++ app/src/main/kotlin/com/spcore/activities/InvitationActivity.kt
@@ -7,6 +7,7 @@ import com.spcore.R
 import com.spcore.adapters.UserProfileListAdapter
 import com.spcore.helpers.*
 import com.spcore.helpers.arListUsers
+import com.spcore.models.User
 import kotlinx.android.synthetic.main.activity_invitations.*
 import org.jetbrains.anko.sdk25.coroutines.onClick
 
@@ -17,28 +18,29 @@ class InvitationActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_invitations)
 
+        val arrayListOfSearchedUsers = ArrayList<User>()
+        invitation_lv.adapter = UserProfileListAdapter(this@InvitationActivity, arrayListOfSearchedUsers)
         done.onClick {
-            performSearh()
+            performSearh(arrayListOfSearchedUsers)
+        }
+        event_crud_cancel_button.onClick {
+            finish()
         }
     }
 
-    private fun performSearh(){
+    private fun performSearh(arrayList: ArrayList<User>){
+        // dummy data
         arListUsers.add(NatoshiSakamoto)
         arListUsers.add(NatoshiSakamoto1)
         arListUsers.add(NatoshiSakamoto2)
         arListUsers.add(NatoshiSakamoto3)
 
-
-        if(userSearch.text == null)
+        if(userSearch.text.isNullOrBlank())
             finish()
         else {
-            arListUsers.forEach {
-                if(it.username == userSearch.text.toString()){
-                    invitation_lv.adapter = UserProfileListAdapter(this@InvitationActivity, arListUsers)
-                }
-
-            }
-
+            val searchedUsers = arListUsers.find { it.username == userSearch.textStr }
+            searchedUsers?.let { arrayList.add(it) }
+            invitation_lv.invalidateViews()
         }
 
     }
