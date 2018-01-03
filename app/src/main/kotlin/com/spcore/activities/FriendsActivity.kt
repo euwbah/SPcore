956778dbe64603f6765f5395418307069ed0d089
euwbah <euwbah@gmail.com>
2018-01-03 17:10:16 +0800

    Implemented prev commit for friend_list_view in FriendsActivity as well

--- app/src/main/kotlin/com/spcore/activities/FriendsActivity.kt
+++ app/src/main/kotlin/com/spcore/activities/FriendsActivity.kt
@@ -10,6 +10,7 @@ import kotlinx.android.synthetic.main.activity_friends.*
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.async
 import org.jetbrains.anko.coroutines.experimental.bg
+import org.jetbrains.anko.startActivity
 
 class FriendsActivity : AppCompatActivity() {
 
@@ -36,6 +37,9 @@ class FriendsActivity : AppCompatActivity() {
         friend_list_view.setOnItemClickListener listener@ { adapterView, view, i, l ->
             val user = view.tag as? User ?: return@listener
 
+            // Don't let the user click oneself's profile
+            if (user != Auth.user)
+                startActivity<FriendScheduleActivity>("user" to user)
         }
     }
 }
